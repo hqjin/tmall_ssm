a65724b829cd944a690d88c6673510f67bf2e1b9
hqjin <hqjin@foxmail.com>
2019-03-21 10:16:06 +0800

    mark

--- src/main/java/com/hqjin/tmall/comparator/ProductAllComparator.java
+++ src/main/java/com/hqjin/tmall/comparator/ProductAllComparator.java
@@ -6,7 +6,7 @@ import java.util.Comparator;
 
 //把 销量x评价 高的放前面
 public class ProductAllComparator implements Comparator<Product> {
-    //升序排序，返回o2-o1
+    //降序排序，返回o2-o1
     @Override
     public int compare(Product o1, Product o2) {
         return o2.getSaleCount()*o2.getReviewCount()-o1.getSaleCount()*o1.getReviewCount();
